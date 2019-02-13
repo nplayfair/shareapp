84a97c6efbb58ab8ba28af59528ddc51103b7720
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-13 17:17:57 +0000

    Implement main view
    Use stylesheets. Include navbar with links for register/login and shares.

--- views/home/index.php
+++ views/home/index.php
@@ -1,1 +1,5 @@
-THIS IS HOME/INDEX
+<div class="text-center">
+    <h1>Welcome to Shareboard</h1>
+    <p class="lead">Found something cool online? Share it with our community.</p>
+    <a class="btn btn-primary" href="<?php echo ROOT_URL; ?>shares">Share Now</a>
+</div>
--- views/main.php
+++ views/main.php
@@ -4,9 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <title>Shareboard</title>
 </head>
 <body>
-    <?php require($view); ?>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Shareboard</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbars">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>shares">Shares</a>
+        </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>users/login">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>users/register">Register</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+
+    <main role="main" class="container">
+
+    <div class="row py-3">
+        <div class="col-md-12 text-center">
+            <?php require($view); ?>
+        </div>
+    </div>
+
+    </main>
 </body>
 </html>
