a3ebe6741bf5f46ca5bfc40c96658be1d0abc478
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-13 16:42:56 +0000

    Index method returns rows for use in view

--- models/share.php
+++ models/share.php
@@ -3,6 +3,6 @@ class ShareModel extends Model {
     public function index() {
         $this->query('SELECT * FROM shares');
         $rows = $this->resultSet();
-        print_r($rows);
+        return $rows;
     }
 }
