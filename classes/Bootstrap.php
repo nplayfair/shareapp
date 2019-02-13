013ee833f00d4c3dc5efece95881258d74219252
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-13 13:12:30 +0000

    rename bootstrap class file

---
+++ classes/Bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+class Bootstrap {
+    private $controller;
+    private $action;
+    private $request;
+
+    public function __construct($request) {
+        // Handle request
+        $this->request = $request;
+        if ($this->request['controller' == '']) {
+            // Load home page
+            $this->controller = 'home';
+        } else {
+            // Load requested controller
+            $this->controller = $this->request['controller'];
+        }
+
+        // Handle action
+        if ($this->request['action'] == '') {
+            // Index action
+            $this->action = 'index';
+        } else {
+            // Action
+            $this->action = $this->request['action'];
+        }
+    }
+}
