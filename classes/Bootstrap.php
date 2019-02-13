d5d489f43941d9c59e1b9e518d79acc6d940a2f7
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-13 15:50:42 +0000

    method to instantiate controllers

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -23,6 +23,32 @@ class Bootstrap {
             // Action
             $this->action = $this->request['action'];
         }
-        
+
+        // Create controller
+        public function createController() {
+            // Check class
+            if (class_exists($this->controller)) {
+                $parents = class_parents($this->controller);
+                // Check extend
+                if (in_array("Controller", $parents)) {
+                    // Does it contain an action?
+                    if (method_exists($this->controller, $this->action)) {
+                        return new $this->controller($this->action, $this->request);
+                    } else {
+                        // Method does not exist
+                        echo '<h1>Method does not exist</h1>';
+                        return;
+                    }
+                } else {
+                    // Base controller does not exist
+                    echo '<h1>Base controller not found</h1>';
+                    return;
+                }
+            } else {
+                // Controller class does not exist
+                echo '<h1>Controller class does not exist</h1>';
+                return;
+            }
+        }
     }
 }
