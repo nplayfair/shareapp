fb7a7e2fdb7fdf7e261053dee9b4b63de5a4eef2
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-17 17:33:25 +0000

    Add share function in model with test value for ID
    Because the user login system is not yet implemented, use a value of 1 for the user_id for testing purposes.

--- models/share.php
+++ models/share.php
@@ -5,4 +5,29 @@ class ShareModel extends Model {
         $rows = $this->resultSet();
         return $rows;
     }
+
+    public function add() {
+        // Sanitise post array
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        // Check for submission
+        if ($post['submit']) {
+            // Insert into DB
+            $this->query('INSERT INTO shares (title, body, link, user_id) VALUES 
+                        (:title, :body, :link, :user_id)');
+            $this->bind(':title', $post['title']);
+            $this->bind(':body', $post['body']);
+            $this->bind(':link', $post['link']);
+            $this->bind(':user_id', 1);
+
+            $this->execute();
+
+            // Verify
+            if ($this->lastInsertId()) {
+                // Redirect
+                header('Locations: '.ROOT_URL.'shares');
+            }
+        }
+        return;
+    }
 }
