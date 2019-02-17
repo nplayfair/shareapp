c96e3a78baccd47f68ad12fe04658c6564f3ff9e
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-17 17:03:05 +0000

    Use root_url const to find assets

--- views/main.php
+++ views/main.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/styles.css">
     <title>Shareboard</title>
 </head>
 <body>
