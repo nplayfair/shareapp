ead76f85ca55be213cb20e0af2c94ce1dadbc71d
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-17 19:25:28 +0000

    Check password matches and set session variables

--- models/user.php
+++ models/user.php
@@ -25,4 +25,41 @@ class UserModel extends Model {
         }
         return;
     }
+
+    public function login() {
+        // Sanitise post array
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        //$password = password_hash($post['password'], PASSWORD_DEFAULT);
+        
+        // Check for submission
+        if ($post['submit']) {
+            // Compare login
+            $this->query('SELECT * from users WHERE email = :email');
+            $this->bind(':email', $post['email']);
+
+            $this->execute();
+
+            // Check password
+            $row = $this->single();
+            // Ensure row is returned before continuing
+            if (!isset($row)) {
+                die('Database Error');
+            }
+            if (password_verify($post['password'], $row['password'])) {
+                // Logged in
+                $_SESSION['is_logged_in'] = true;
+                $_SESSION['user_data'] = array(
+                    "id"    => $row['id'],
+                    "name"  => $row['name'],
+                    "email" => $row['email']
+                );
+                header('Location: '.ROOT_URL.'shares');
+            } else {
+                // Incorrect
+                echo 'Incorrect login';
+            }
+        }
+        return;
+    }
 }
