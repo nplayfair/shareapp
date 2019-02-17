1fece62a6fc233dd53db278baa78bdc73c2dad80
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-17 17:31:36 +0000

    Implement function to return last insert id

--- classes/Model.php
+++ classes/Model.php
@@ -39,4 +39,8 @@ abstract class Model {
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function lastInsertId() {
+        return $this->dbh->lastInsertId();
+    }
+
 }
