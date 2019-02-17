119dea0732f3df3193cd179859d3a98b65b1cbe2
Nick Playfair <nick@chaosemerald.co.uk>
2019-02-17 16:28:50 +0000

    Shares view

--- views/shares/index.php
+++ views/shares/index.php
@@ -1,1 +1,15 @@
-THIS IS SHARES/INDEX
+<div class="container">
+    <a href="<?php echo ROOT_URL; ?>shares/add" class="btn btn-success btn-share">Share Something</a>
+    <?php foreach($viewmodel as $item) : ?>
+        <div class="card my-2 text-left">
+            <div class="card-body">
+                <h3 class="card-title"><?php echo $item['title']; ?></h3>
+                <p><?php echo $item['body']; ?></p>
+                <a href="<?php echo $item['link']; ?>" class="btn btn-primary" target="_blank">Go to Link</a>
+            </div>
+            <div class="card-footer text-muted">
+                <small>Shared on <?php echo $item['create_date']; ?></small>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
